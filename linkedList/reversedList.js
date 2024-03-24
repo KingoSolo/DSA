8e09c4aa3d5c301f28f9ecb11f11a30be8855577
Adeniran Solomon <soluwaseun806@gmail.com>
2024-03-24 01:27:05 +0100

    Update reversedList.js

--- linkedList/reversedList.js
+++ linkedList/reversedList.js
@@ -1,5 +1,21 @@
+//Given the head of a singly linked list, reverse the list, and return the reversed list.
 class Node{
     constructor(element){
-        t
+        this.element = element
+        this.next=null
     }
+}
+
+function reversedList(head){
+    let prev = null
+    let current = head 
+
+    while(current !== null){
+        const nextNode = current.next
+        current.next = prev
+        prev = current 
+        current = nextNode
+    }
+return prev
+
 }
