b7ba9099d243d942c724525a6e3ec26be4694df6
Adeniran Solomon <soluwaseun806@gmail.com>
2024-05-09 13:53:18 +0100

    Update crossingPath.js

---
+++ hashmap/crossingPath.js
@@ -0,0 +1,25 @@
+var isPathCrossing = function(path) {
+    
+    const hashMap = new Map()
+    let currentX = 0
+    let currentY = 0
+    hashMap.set(`${currentX},${currentY}`,true)
+
+    for(let i = 0; i < path.length; i++){
+        if(path[i] === "N") currentY++
+         else if(path[i] === "S") currentY--
+          else if(path[i] === "E") currentX++
+           else currentX--
+
+        const points = `${currentX},${currentY}`
+        if(hashMap.has(points)){
+            return true
+        }
+        hashMap.has(points,true)
+    }
+    return false
+
+}
+
+
+
